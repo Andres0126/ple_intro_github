eb16687bd9ef39d11e2b6343854031f11daa63d2
Andres0126 <acalderongu@unal.edu.co>
2025-05-02 00:14:51 -0500

    Actualizar

--- Taller #3 pasar codigo python a R_Tablas.R
+++ Taller #3 pasar codigo python a R_Tablas.R
@@ -1,4 +1,4 @@
-# No necesitas importar librerías para data.frame básico
+# No necesito importar librerias en R
 conos <- data.frame(
   Sabor = c('mora', 'vainilla', 'chocolate', 'cebolla', 'chococrispi'),
   precio = c(3500, 4700, 6500, 5200, 5700)
@@ -7,20 +7,20 @@ conos <- data.frame(
 # Mostrar el data frame
 print(conos) #Imprime la tabla
 conos #Imprime la tabla
-conos[1:3, ] #En R, el índice empieza en 1 (no en 0) y sí incluye el último número, por tanto imprime los 3 valores de la tabla
+conos[1:3, ] #En R, el ?ndice empieza en 1 (no en 0) y s? incluye el ?ltimo n?mero, por tanto imprime los 3 valores de la tabla
 
-#En R dentro de [] la primera parte antes de la coma "," se refiere a las filas , y la segunda parte después de la coma se refiere a las columnas .
+#En R dentro de [] la primera parte antes de la coma "," se refiere a las filas , y la segunda parte despu?s de la coma se refiere a las columnas .
 conos[, c('Sabor', 'precio')] 
 conos[order(conos$Sabor), ] #Genera el orden correcto de las filas calculadas en el valor de "Sabor".
 conos[order(conos$Sabor, decreasing = TRUE), ] #Genera el orden de forma descendente (Z???A)
 
-head(conos, 2) #significa:Muéstrame las primeras "2" filas de conos.
-tail(conos, 2)#significa:Muéstrame las últimas 2 filas deconos
+head(conos, 2) #significa:Mu?strame las primeras "2" filas de conos.
+tail(conos, 2)#significa:Mu?strame las ?ltimas 2 filas deconos
 
 # Seleccionar una fila aleatoria de la tabla "conos"
 
-nrow(conos) #devuelve el número total de filas en el DataFrame conos
-sample(nrow(conos), 1) #selecciona aleatoriamente un número entre 1 y el número total de filas.
+nrow(conos) #devuelve el n?mero total de filas en el DataFrame conos
+sample(nrow(conos), 1) #selecciona aleatoriamente un n?mero entre 1 y el n?mero total de filas.
 #El 1 indica a R que escoja la primera fila aleatoria de las x filas que tiene el vector
 
 #Recuerda que dentro de un vector [,] la coma separa las filas de las columnas
@@ -29,20 +29,20 @@ conos[sample(nrow(conos), ), ] #Estamos arrojando la tabla con los datos al azar
 conos[sample(nrow(conos), 1), ] #Estamos arrojando un solo dato de la tabla al azar.
 #Esto se debe a que sample(,) esta seleccionando datos aleatorios del indice y 1 solo de las columnas
 
-conos[sample(nrow(conos), 1)] #R no sabe si estás seleccionando una fila o una columna.
-#Sin la coma , R trataría de seleccionar una columna , no una fila, y te daría un error o un resultado incorrecto.
+conos[sample(nrow(conos), 1)] #R no sabe si est?s seleccionando una fila o una columna.
+#Sin la coma , R tratar?a de seleccionar una columna , no una fila, y te dar?a un error o un resultado incorrecto.
 
 summary(conos) #Entrega todos los datos de abajo
-#Devuelve valores como: count, mean, std, min, 25%, 50%, 75%, max para las columnas numéricas ( precio).
+#Devuelve valores como: count, mean, std, min, 25%, 50%, 75%, max para las columnas num?ricas ( precio).
 
-str(conos) # Muestra el tipo de objeto ( data.frame), el número, el tipo de dato de cada columna ( Factor, Numeric,Character, etc.), los primeros valores de cada columna
+str(conos) # Muestra el tipo de objeto ( data.frame), el n?mero, el tipo de dato de cada columna ( Factor, Numeric,Character, etc.), los primeros valores de cada columna
 
-#R no maneja indices, El índice son implícitamente las filas : 1, 2, 3, 4, etc.
+#R no maneja indices, El ?ndice son impl?citamente las filas : 1, 2, 3, 4, etc.
 
-rownames(conos) #Acceder a los números de fila 
+rownames(conos) #Acceder a los n?meros de fila 
 as.numeric(rownames(conos)) #Si queremos los indices como numeros 
 
-conos[4, ] #ubicación por índice numérico.
+conos[4, ] #ubicaci?n por ?ndice num?rico.
 
 #Seleccione la fila 1 en adelante, la fila 2 a la 4 y las fila 1 y 4.
 
@@ -64,12 +64,12 @@ conos[c(1,4), ]
 conos[5, "Sabor"]
 
 
-# Cambiar el indice númerico por la columna "Sabor"
+# Cambiar el indice n?merico por la columna "Sabor"
 
 conos_i <- conos #Copiamos el data.frame original
-#make.unique() agregará .1, .2, etc., a los nombres repetidos
-rownames(conos_i) <- make.unique(as.character(conos_i$Sabor)) ##Creamos índices únicos a partir de la columna "Sabor"
-conos_i$Sabor <- NULL #Eliminamos la columna "Sabor" para que no esté repetida
+#make.unique() agregar? .1, .2, etc., a los nombres repetidos
+rownames(conos_i) <- make.unique(as.character(conos_i$Sabor)) ##Creamos ?ndices ?nicos a partir de la columna "Sabor"
+conos_i$Sabor <- NULL #Eliminamos la columna "Sabor" para que no est? repetida
 conos_i#Mostramos el resultado
 conos_i["chococrispi", ]
 
